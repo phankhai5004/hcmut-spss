926bb40482aee85e93a6e76606828a78df1df3c6
phankhai5004 <phankhai5004@gmail.com>
2024-11-07 22:02:57 +0700

    feat: implemented topbar and sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ export default function App() {
   return (
     <>
       <h1 className="bg-red-700">ajsdlfjdalkjdflkajlkfajdslkjf</h1>
-      <ajdslkfjadlksjflkadjlkjfla></ajdslkfjadlksjflkadjlkjfla>
     </>
   );
 }
---
+++ src/constants/PageName.ts
@@ -0,0 +1,11 @@
+type PageNameMap = {
+  [key: string]: string;
+};
+
+export const PageName: PageNameMap = {
+  '/': 'Bảng điều khiển',
+  '/print': 'In tài liệu',
+  '/history': 'Lịch sử',
+  '/printer-list': 'Danh sách máy in',
+  '/notification': 'Thông báo',
+};
